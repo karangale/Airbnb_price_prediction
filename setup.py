2eb972e1fbdbc3203232031d55393fdfc5f22f39
Scarlett Dias <sdias@uw.edu>
2017-12-07 23:47:35 -0800

    Updated setup with required info

--- setup.py
+++ setup.py
@@ -1,22 +1,18 @@
-import os
+"""A setup for the Airbnb price prediction project."""
+
 from setuptools import setup, find_packages
 PACKAGES = find_packages()
 
-# Get version and release info, which is all stored in shablona/version.py
-ver_file = os.path.join('Airbnb-price-prediction', 'version.py')
-with open(ver_file) as f:
-    exec(f.read())
-
 opts = dict(name='Airbnb Price Prediction',
-            description='Predicting Airbnb prices based on the number of restaurants',
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
+            description='Predicting Airbnb prices from the number of '
+            'restaurants',
+            long_description='Analyze the effect of restaurants and eateries '
+            'on Airbnb listings in New York city',
+            url='https://github.com/UWSEDS-aut17/uwseds-group-team-star',
             license=open('LICENSE').read(),
             classifiers=CLASSIFIERS,
-            author='scarlettdias, anirao26, karangale, rjtgupta',
-            platforms=PLATFORMS,
-            version=VERSION,
+            author='scarlettdias, karangale, anirao26, rjtgupta',
+            version='0.1.0',
             packages=PACKAGES,
             package_data={'Airbnb-price-prediction': ['Data/*.*']},
             install_requires=REQUIRES,
