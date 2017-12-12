2a7359901c55af5281e4905a146870374b69a9c2
Anirudh Rao <anirao26@uw.edu>
2017-12-11 20:43:36 -0800

    Restructuring file structure and design

--- Code/ML_model.py
+++ Code/ML_model.py
@@ -14,7 +14,6 @@ plt.style.use('ggplot')
 
 airbnb_data = pd.read_csv("listings.csv", encoding='latin-1')
 
-
 column_names = ["id", "host_listings_count", "host_total_listings_count", "host_has_profile_pic", "host_identity_verified",
             "street", "neighbourhood", "city", "state", "zipcode", "market", "country_code", "country", "latitude", "longitude", "property_type",
              "room_type", "accommodates", "bathrooms", "bedrooms", "beds", "bed_type", "price", "cleaning_fee", "extra_people", "requires_license",
@@ -28,14 +27,8 @@ columns_interest_airbnb = ['street', 'neighbourhood','zipcode', 'latitude', 'lon
                                'number_of_reviews']
 
 airbnb_data_req = airbnb_data[column_names]
-
-
-
 prop_type= airbnb_data_req.groupby('property_type')['property_type'].count()
-# prop_type.plot(kind='bar',figsize=(14,6))
-# plt.show()
 
-#Thus, as the distribution is skewed, we will only be using the property-type with entries as 'apartment'
 
 def oneHotEnconde(df):
     enc = LabelEncoder()
@@ -43,15 +36,13 @@ def oneHotEnconde(df):
     df_1=pd.DataFrame()
 
     for col in ["room_type", "bed_type", "cancellation_policy"]:
-        # df[col] = df[col].astype(str)
         if df[col].dtypes == 'object':
-            # enc.fit(df[[col]].values)
             df[col] =  pd.Series(enc.fit_transform(df[[col]]), index=df.index)
-            # print("\n\n", type(enc.fit_transform(df[[col]])), "\n\n")
             temp = enc1.fit(df[col].values)
             temp = pd.DataFrame(temp.toarray(), columns=[(col+"_"+str(i)) for i in df[col].value_counts().index])
             temp = temp.set_index(df.index.values)
             df_1 = pd.concat([df, temp],axis=1)
+
     return df_1
 
 
@@ -106,7 +97,6 @@ def clean_airbnb(airbnb_data_req):
 
 
 airbnb_data_req = binaryEncode(airbnb_data_req)
-# airbnb_data_req = oneHotEnconde(airbnb_data_req)
 airbnb_data_req = clean_airbnb(airbnb_data_req)
 airbnb_data_req.to_csv('out.csv')
 
@@ -114,10 +104,11 @@ print(airbnb_data_req.columns)
 
 grouped_zipcode = airbnb_data_req.groupby('zipcode')
 
-    x_train = airbnb_data_req[['bedrooms','bathrooms','accommodates','beds','number_of_reviews', 'minimum_nights', 'maximum_nights', 'number_of_reviews', "host_has_profile_pic", "host_identity_verified", "requires_license", "instant_bookable", "is_business_travel_ready", "require_guest_profile_picture", "require_guest_phone_verification"]]
+x_train = airbnb_data_req[['bedrooms','bathrooms','accommodates','beds','number_of_reviews', 'minimum_nights', 'maximum_nights', 
+                            'number_of_reviews', "host_has_profile_pic", "host_identity_verified", "requires_license", "instant_bookable", 
+                            "is_business_travel_ready", "require_guest_profile_picture", "require_guest_phone_verification"]]
 
 y_train = airbnb_data_req['price'].values
-
 
 reg=linear_model.LinearRegression()
 reg.fit(x_train, y_train)
@@ -125,18 +116,13 @@ y_pred = reg.predict(x_train)
 print("Linear regression r squared", r2_score(y_train,y_pred), "\n")
 
 
-reg1=ElasticNet()
-reg1.fit(x_train, y_train)
-y_pred1 = reg1.predict(x_train)
-print("Elastic regression r squared", r2_score(y_train, y_pred1), "\n")
+# reg1=ElasticNet()
+# reg1.fit(x_train, y_train)
+# y_pred1 = reg1.predict(x_train)
+# print("Elastic regression r squared", r2_score(y_train, y_pred1), "\n")
 
 
-reg2=linear_model.Lasso(alpha=.16)
-reg2.fit(x_train, y_train)
-y_pred2 = reg2.predict(x_train)
-print("Lasso regression r squared", r2_score(y_train, y_pred1), "\n")
-
-# coords_1 = (52.2296756, 21.0122287)
-# coords_2 = (52.406374, 16.9251681)
-
-# geopy.distance.vincenty(coords_1, coords_2).km
+# reg2=linear_model.Lasso(alpha=.16)
+# reg2.fit(x_train, y_train)
+# y_pred2 = reg2.predict(x_train)
+# print("Lasso regression r squared", r2_score(y_train, y_pred1), "\n")
