7b5b51b443535888de2af4d2684e326c685ef573
Anirudh Rao <anirao26@uw.edu>
2017-12-12 10:21:42 -0800

    Flask integration

--- Code/Create_frontpage.py
+++ Code/Create_frontpage.py
@@ -1,36 +1,54 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return render_template('new.html')
 
+
 @app.route("/airbnb-restaurant", methods=['GET'])
 def airbnb_restaurant():
     return render_template('airbnb-restaurant.html')
 
-@app.route("/ML_model", methods=['POST'])
+
+@app.route("/get_model_page", methods=['GET'])
+def get_model_page():
+    return render_template('ML_model.html')
+
+
+@app.route("/ML_model", methods=['GET', 'POST'])
 def ML_model():
-
+    
     if request.method == 'POST':    
-	    video_name = request.form['video_name']
-	    channel_title = request.form['channel_title']
-	    video_category = request.form['video_category']
-	    description = request.form['description']
+        n_rooms = request.form['n_rooms']
+        nbeds = request.form['nbeds']
+        naccommodates = request.form['naccommodates']
+        nrestaurants = request.form['nrestaurants']
+        nreviews = request.form['nreviews']
+        nrating = request.form['nrating']
+        region = request.form['region']
 
-    print(video_name)
-    if video_name is None:
-        video_name = ""
-    if channel_title is None:
-        channel_title = ""
-    if video_category is None:
-        video_category = ""
-    if description is None:
-        description = ""
+    if n_rooms is None:
+        n_rooms = ""
+    if nbeds is None:
+        nbeds = ""
+    if naccommodates is None:
+        naccommodates = ""
+    if nrestaurants is None:
+        nrestaurants = ""
+    if nreviews is None:
+        nreviews = ""
+    if nrating is None:
+        nrating = ""
+    if region is None:
+        region = ""
 
-    recommend_tags = rec.initializeAndFetchRecommendations(video_name, channel_title, video_category, description)
-    print("\n\n", recommend_tags)
-    return render_template('ML_model.html', video_name = video_name, channel_title = channel_title, video_category = video_category, description = description, recommendation = recommend_tags)
+    prediction = "prediction"
+    print(region)
+
+    return render_template('ML_model.html', n_rooms = n_rooms, nbeds = nbeds, naccommodates = naccommodates, 
+                                            nrestaurants = nrestaurants,  nreviews = nreviews, nrating = nrating, 
+                                            region = region, prediction = prediction)
 
 
 if __name__=="__main__":
