74d90c2ad0600eba25feac5f1b5634c76898ca35
Scarlett Dias <sdias@uw.edu>
2017-12-14 01:00:47 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 """A setup for the Airbnb price prediction project."""
 
-from setuptools import setup, find_packages
-PACKAGES = find_packages()
+from setuptools import setup
 
-opts = dict(name='Airbnb Price Prediction',
+with open("README", 'r') as f:
+    long_description = f.read()
+
+opts = dict(name='airbnb_price_prediction',
             description='Predicting Airbnb prices from the number of '
             'restaurants',
-            long_description='Analyze the effect of restaurants and eateries '
-            'on Airbnb listings in New York city',
+            long_description=long_description,
             url='https://github.com/UWSEDS-aut17/uwseds-group-team-star',
             license=open('LICENSE').read(),
-            classifiers=CLASSIFIERS,
             author='scarlettdias, karangale, anirao26, rjtgupta',
             version='0.1.0',
-            packages=PACKAGES,
+            packages=['airbnb_price_prediction'],
             package_data={'Airbnb-price-prediction': ['Data/*.*']},
-            install_requires=REQUIRES,
-            requires=REQUIRES)
+            install_requires=['flask', 'numpy', 'math', 'os', 'pandas',
+                              'sklearn', 'matplotlib'])
 
 
 if __name__ == '__main__':
