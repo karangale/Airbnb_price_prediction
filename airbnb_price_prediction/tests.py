91f37102deffb2b85da8e88e81b572d18bd136d5
Anirudh Rao <anirao26@uw.edu>
2017-12-13 22:09:20 -0800

    committing test cases for clean

--- airbnb_price_prediction/tests.py
+++ airbnb_price_prediction/tests.py
@@ -1,44 +1,36 @@
 import unittest
 import pandas as pd
+import cleaning_utils as cu
 
-from cleaning import splitZip
-from cleaning import rest_name
-from cleaning import rest_add
-from cleaning import clean_airbnb
-from cleaning import clean_restraunts_geo
+REST_FILE_SHAPE = (24941, 6)
 
 class TestCleaningFunctions(unittest.TestCase):
 
     def test_spitZip(self):
         testzip = "11426.0-1175"
         actualzip = "11426"
-        self.assertEqual(splitZip(testzip), actualzip)
+        self.assertEqual(cu.splitZip(testzip), actualzip)
+
 
     def test_rest_name(self):
         comb_name = "Restaurant Name 11(Buliding Number) Address Zipcode"
         actual_name = "Restaurant Name"
-        self.assertEqual(rest_name(comb_name), actual_name)
+        self.assertEqual(cu.rest_name(comb_name), actual_name)
+
 
     def test_add(self):
         row = pd.DataFrame(columns = ['Restaurant', 'Name'])
         row.loc[0] = ["Restaurant Name 11(Buliding Number) Address Zipcode", 
                       "Restaurant Name"]
         address = " 11(Buliding Number) Address Zipcode"
-        add = row.apply(rest_add,axis=1)[0]
+        add = row.apply(cu.rest_add,axis=1)[0]
         self.assertEqual(add, address)
 
-    def test_clean_airbnb(self):
-        final_shape = (36987, 17)
-        fileDetails = pd.read_csv("./Data/listings.csv")
-        final_df = clean_airbnb(fileDetails)
-        self.assertEqual(final_df.shape, final_shape)
 
     def test_clean_restaurants_go(self):
-        final_shape = (24941, 6)
-        fileDetails = pd.read_csv(
-            "./Data/NYC Restaurants Geocoded - Sheet1.csv")
-        final_df = clean_restraunts_geo(fileDetails)
-        self.assertEqual(final_df.shape, final_shape)
+        fileDetails = pd.read_csv("./Data/NYC Restaurants Geocoded - Sheet1.csv")
+        final_df = cu.clean_restraunts_geo(fileDetails)
+        self.assertEqual(final_df.shape, REST_FILE_SHAPE)
 
 
 if __name__=="__main__":
