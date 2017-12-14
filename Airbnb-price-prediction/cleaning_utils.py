620cdc9624843405f1be75f3817b298cd2141aba
Anirudh Rao <anirao26@uw.edu>
2017-12-13 21:49:49 -0800

    Added documentation for ML model and clean functions

---
+++ Airbnb-price-prediction/cleaning_utils.py
@@ -0,0 +1,127 @@
+LATITUDE = 41
+LONGITUDE_LOWER = -75
+LONGITUDE_UPPER = -73.5
+NUM_LIST = ['0','1','2','3','4','5','6','7','8','9','-', '.']
+
+
+def splitZip(zipcode):
+    
+    '''
+    This function removes the special characters present in the zip codes.
+
+    Params:
+        zipcode: (alphanumeric) The zipcode which needs to be processed to conform to a standard format
+    Returns:
+        The zipcode after removing special characters
+    '''
+    zips = zipcode.split("-")
+    zipsDot = zips[0].split(".")
+    zipsN = zipsDot[0].split("\n")
+    return zipsN[0]
+
+
+def clean_airbnb(airbnb_data_req):
+    
+    '''
+    This function is for cleaning up the airbnb data.
+    It handles multiple use cases like null handling, imputing empty cell data etc.
+    It also removes rows for which there is skewed representation in the data.
+    Removes the special characters from amount fields.
+
+    Params:
+        airbnb_data_req: (dataframe) The dataframe that needs to be cleaned
+    Returns:
+        A dataframe after the cleaning has been performed.
+    '''
+
+    index_drop = airbnb_data_req[airbnb_data_req['zipcode'].isnull()].index.tolist()
+    airbnb_data_req = airbnb_data_req.drop(index_drop)
+    airbnb_data_req = airbnb_data_req.reset_index(drop = True)
+
+    airbnb_data_req["bathrooms"].fillna(0, inplace = True)
+    airbnb_data_req["bedrooms"].fillna(0, inplace = True)
+    airbnb_data_req["beds"].fillna(0, inplace = True)
+    airbnb_data_req["neighbourhood"].fillna("", inplace = True)
+
+    airbnb_data_req["price"] = airbnb_data_req["price"].str.replace('$', '')
+    airbnb_data_req["price"] = airbnb_data_req["price"].str.replace(',', '')
+    airbnb_data_req["price"] = airbnb_data_req["price"].astype(float)
+
+    airbnb_data_req["cleaning_fee"] = airbnb_data_req["cleaning_fee"].str.replace('$', '')
+    airbnb_data_req["cleaning_fee"] = airbnb_data_req["cleaning_fee"].str.replace(',', '')
+    airbnb_data_req["cleaning_fee"] = airbnb_data_req["cleaning_fee"].astype(float)
+
+    airbnb_data_req["extra_people"] = airbnb_data_req["extra_people"].str.replace('$', '')
+    airbnb_data_req["extra_people"] = airbnb_data_req["extra_people"].str.replace(',', '')
+    airbnb_data_req["extra_people"] = airbnb_data_req["extra_people"].astype(float)
+
+    airbnb_data_req["zipcode"] = airbnb_data_req["zipcode"].astype(str)
+
+    airbnb_data_req["zipcode"] = airbnb_data_req["zipcode"].apply(splitZip)
+    airbnb_data_req= airbnb_data_req[airbnb_data_req['zipcode']!='1m']
+
+    airbnb_data_req = airbnb_data_req[airbnb_data_req['property_type'] == 'Apartment']
+    airbnb_data_req = airbnb_data_req[airbnb_data_req['country_code'] == "US"]
+
+    return airbnb_data_req
+
+
+def rest_name(rest_name):
+    '''
+    This function checks if the .
+
+    Params:
+        row_data: (dataframe row) A row of the restaurant data
+    Returns:
+        A row with the transformed data
+    '''
+    i = len(rest_name)
+    restaurant = rest_name[:(i-6)]
+    length = len(restaurant)
+    for j in range((length-1), 0, -1):
+        if(restaurant[j] in NUM_LIST):
+            if((restaurant[j-1] == " ")and(restaurant[j-2] not in NUM_LIST)):
+                break;
+            else:
+                continue
+
+    name = restaurant[:(j-1)]
+
+    if(name==""):
+        name = rest_name[:len(rest_name)-17]
+
+    return name
+
+
+def rest_add(row_data):
+    '''
+    This function applies a row level transformation to the restaurant data to return the restaurant name.
+
+    Params:
+        row_data: (dataframe row) A row of the restaurant data
+    Returns:
+        A row with the transformed data
+    '''
+
+    add = row_data['Restaurant'].split(row_data['Name'])[1]
+    return add
+
+    
+def clean_restraunts_geo(restaurant_data):
+    '''
+    This function limits the geography of the data to around New York.
+
+    Params:
+        restaurant_data: (dataframe) The restaurant dataframe to be cleaned
+    Returns:
+        The cleaned restaurant dataframe
+    '''
+    restaurant_data.dropna()
+    restaurant_data = restaurant_data[restaurant_data['Latitude']<LATITUDE]
+    restaurant_data = restaurant_data[restaurant_data['Longitude']>LONGITUDE_LOWER]
+    restaurant_data = restaurant_data[restaurant_data['Longitude']<LONGITUDE_UPPER]
+
+    restaurant_data['Name'] = restaurant_data['Restaurant'].apply(rest_name)
+    restaurant_data['Address'] = restaurant_data.apply(rest_add, axis=1)
+
+    return restaurant_data
---
+++ Airbnb-price-prediction/ml_model.py
@@ -0,0 +1,141 @@
+import os
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+from sklearn.metrics import r2_score
+from sklearn import linear_model
+from sklearn.linear_model import ElasticNet
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+import cleaning_utils
+plt.style.use('ggplot')
+
+
+ONE_HOT_ENCODE_COLUMNS = ["room_type", "bed_type", "cancellation_policy"]
+BINARY_ENCODE_COLUMNS = ["host_has_profile_pic", "host_identity_verified", "requires_license", "instant_bookable", 
+                         "is_business_travel_ready", "require_guest_profile_picture", "require_guest_phone_verification"]
+
+LINEAR_REGRESSION_FEATURES = ['bedrooms', 'beds', 'accommodates', "rest_count", 'number_of_reviews', 'minimum_nights', 'maximum_nights']
+
+
+def read_data(path, airbnb_file_name, restaurant_data_file_name):
+    
+    '''
+    This function reads the airbnb and restaurant data from the data directory.
+    You must have listings.csv and rest_counts.csv files within the Data directory for this function to work.
+
+    Params:
+        path: (str) This is the path to the data file
+    Returns:
+        two dataframes, one with airbnb data and the other with the restaurant data
+    '''
+
+    airbnb_data_file_path = os.path.join(path, 'Data', airbnb_file_name)
+    restaurant_data_file_path = os.path.join(path, 'Data', restaurant_data_file_name)
+
+    airbnb_data = pd.read_csv(airbnb_data_file_path, encoding='latin-1')
+    restaurant_data = pd.read_csv(restaurant_data_file_path, encoding='latin-1')
+
+    return airbnb_data, restaurant_data
+
+
+def oneHotEncode(df, col_names):
+
+    '''
+    This function is for specifically converting categorical variables into numerical features. This is done using the one hot encoding implementation
+    within scikit-learn. It increases the number of features by the number of unique categories for the specified categorical variables ("room_type", 
+    "bed_type", and "cancellation_policy" in this case). It ensures no category gets more weightage than others after numerical representation. 
+    Particularly useful for features with more than two categories.
+
+    Params:
+        df: (dataframe) This is the dataframe on which the one hot encoding needs to be performed.
+        col_names: (list) A list with the desired column names that need to be considered for one hot encoding
+    Returns:
+        a dataframe with additional features added for every category within columns specified in the col_names argument
+    '''
+
+    enc = LabelEncoder()
+    enc1 = OneHotEncoder()
+    df_1 = pd.DataFrame()
+
+    for col in col_names:
+        if df[col].dtypes == 'object':
+            df[col] =  pd.Series(enc.fit_transform(df[[col]]), index=df.index)
+            temp = enc1.fit(df[col].values)
+            temp = pd.DataFrame(temp.toarray(), columns=[(col+"_"+str(i)) for i in df[col].value_counts().index])
+            temp = temp.set_index(df.index.values)
+            df_1 = pd.concat([df, temp],axis=1)
+
+    return df_1
+
+
+def binaryEncode(df, col_names):
+    
+    '''
+    This function is for specifically converting categorical variables into numerical features. This implementation is specifically for categorical 
+    variables with two categories. Does not add any new features for categories. Alters the existing column to a 0/1 representation for the two 
+    categories.
+
+    Params:
+        df: (dataframe) This is the dataframe on which binary encoding needs to be performed.
+        col_names: (list) A list with the desired column names that need to be considered for binary encoding
+    Returns:
+        a dataframe with columns specified in the col_names argument as encoded as 0/1 for the two categories
+
+    '''
+
+    for i in col_names:
+        df[i] = df[i].apply(lambda x: 1 if x =='y' else 0)
+
+    return df
+
+
+def build_linear_regression_model(feature_list): 
+    
+    '''
+    This function builds the Linear Regression model used later for predicting listing prices.
+    Only features of interest are used for building the model. 
+    The two data sets are joined on the id column and this dataset is used later for further analysis.
+
+    Params:
+        feature_list: (list) List of features used to build the Linear Regression model
+    Returns:
+        A model object with the data fit on the training data
+    '''
+
+    current_dir_path = os.getcwd()
+    airbnb_data_req, restaurant_data = read_data(current_dir_path, 'listings.csv', 'rest_count.csv')
+    airbnb_data_req = binaryEncode(airbnb_data_req, BINARY_ENCODE_COLUMNS)
+    airbnb_data_req = cleaning_utils.clean_airbnb(airbnb_data_req)
+    # restaurant_data = cleaning_utils.clean_restraunts_geo(restaurant_data)
+
+    airbnb_data_req = airbnb_data_req.join(restaurant_data, on="id", how = "inner") #Merging the two datasets on id
+    x_train = airbnb_data_req[feature_list]
+    y_train = airbnb_data_req['price'].values
+
+    reg = linear_model.LinearRegression()
+    reg.fit(x_train, y_train)
+    y_pred = reg.predict(x_train)
+    print("Linear regression r squared", r2_score(y_train,y_pred), "\n")
+    return reg
+
+
+def predict_price(data):
+    
+    '''
+    This function is used for making predictions on the test data. 
+    It calls the build_linear_regression_model() function to build the linear regression model and makes the predictions for the data argument 
+    passed to the function as an argument.
+
+    Params:
+        data: (numpy array) An instance of data (with the features) for which the prediction has to be made 
+    Returns:
+        A floating point number corresponding to the predicted value
+    '''
+
+    model = build_linear_regression_model(LINEAR_REGRESSION_FEATURES)
+    return model.predict(data)
+
+if __name__ == '__main__':
+    build_linear_regression_model(LINEAR_REGRESSION_FEATURES)
